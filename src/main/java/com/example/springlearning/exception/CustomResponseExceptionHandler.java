3147079d38e6647f3260135d7e48681b63b19967
asma <asma@cefalo.com>
2023-07-07 18:07:25 +0600

    update validation

--- src/main/java/com/example/springlearning/exception/CustomResponseExceptionHandler.java
+++ src/main/java/com/example/springlearning/exception/CustomResponseExceptionHandler.java
@@ -7,31 +7,29 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
-    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) {
-        CustomizedErrorResponse errorResponse = new
-                CustomizedErrorResponse(LocalDateTime.now(), ex.getMessage(), HttpStatus.NOT_FOUND.value());
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    public final CustomizedErrorResponse handleUserNotFoundException(Exception ex, WebRequest request) {
+        return new CustomizedErrorResponse(LocalDateTime.now(), ex.getMessage());
     }
 
-    @Override
     @Nullable
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         CustomizedErrorResponse errorResponse = new CustomizedErrorResponse
-                (LocalDateTime.now(), Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(), HttpStatus.NOT_FOUND.value());
-
+                (LocalDateTime.now(), Objects.requireNonNull(ex.getFieldError()).getDefaultMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
--- src/main/java/com/example/springlearning/model/User.java
+++ src/main/java/com/example/springlearning/model/User.java
@@ -1,17 +1,25 @@
 package com.example.springlearning.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Entity(name="user_details")
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 character")
     private String name;
